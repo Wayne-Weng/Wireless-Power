cca2c8373743efcf78304bdce715d5f1bd40e77e
Wayne-Weng <cmdjbst@gmail.com>
2013-10-05 02:55:36 +0800

    Bug fix.
    Fixed startup integral error.

---
+++ STM32F1_RNR_PID_WPC_20131004/ProjectNR/Obj/system_stm32f10x.d
@@ -0,0 +1,13 @@
+.\obj\system_stm32f10x.o: ..\Libraries\CMSIS\system_stm32f10x.c
+.\obj\system_stm32f10x.o: ..\Libraries\CMSIS\stm32f10x.h
+.\obj\system_stm32f10x.o: ..\Libraries\CMSIS\core_cm3.h
+.\obj\system_stm32f10x.o: E:\Program Files\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\obj\system_stm32f10x.o: ..\Libraries\CMSIS\system_stm32f10x.h
+.\obj\system_stm32f10x.o: ..\Program\stm32f10x_conf.h
+.\obj\system_stm32f10x.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_adc.h
+.\obj\system_stm32f10x.o: ..\Libraries\CMSIS\stm32f10x.h
+.\obj\system_stm32f10x.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_dma.h
+.\obj\system_stm32f10x.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\obj\system_stm32f10x.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\obj\system_stm32f10x.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_tim.h
+.\obj\system_stm32f10x.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\misc.h
